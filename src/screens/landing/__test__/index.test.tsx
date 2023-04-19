9bb185d1bfa783babf090c95fe72845cc16b94eb
Garri Adrian Nablo <garri.almighty@gmail.com>
2023-04-19 18:51:41 +0800

    tech(redux): setup redux and store

--- src/screens/landing/__test__/index.test.tsx
+++ src/screens/landing/__test__/index.test.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import {Provider} from 'react-redux';
+import configureStore from 'redux-mock-store';
 import {render} from '@testing-library/react-native';
 
 import LandingScreen from '../';
 
+const mockStore = configureStore();
+const state = {
+  category: {
+    categories: [],
+  },
+};
+const store = mockStore(state);
+
 describe('Screen: Landing', () => {
   it('should render properly', () => {
-    const {toJSON} = render(<LandingScreen />);
+    const {toJSON} = render(
+      <Provider store={store}>
+        <LandingScreen />
+      </Provider>,
+    );
     expect(toJSON).toMatchSnapshot();
   });
 });
--- src/screens/landing/index.tsx
+++ src/screens/landing/index.tsx
@@ -1,19 +1,24 @@
-import React from 'react';
+import React, {useMemo} from 'react';
 import {Button, SafeAreaView, Text, View} from 'react-native';
 
+import {useAppSelector} from 'src/hooks/useAppSelector';
 import CategoryButton from 'src/components/category-button';
 
 import styles from './styles';
 
 const LandingScreen = (): JSX.Element => {
+  const categories = useAppSelector(state => state.category.categories);
+  const CategoryButtons = useMemo(() => {
+    // convert to a FlatList once the design evolves
+    return categories.map(({name}) => (
+      <CategoryButton key={name} title={name} style={styles.categoryButton} />
+    ));
+  }, [categories]);
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Words Puzzle</Text>
-      <View style={styles.buttonContainer}>
-        <CategoryButton style={styles.categoryButton} title="Cities" />
-        <CategoryButton style={styles.categoryButton} title="Food" />
-        <CategoryButton style={styles.categoryButton} title="Animals" />
-      </View>
+      <View style={styles.buttonContainer}>{CategoryButtons}</View>
       <CategoryButton style={styles.startButton} title="Start" />
       <Button onPress={() => {}} title="Leaderboards" />
     </SafeAreaView>
